d7c63296b4006c70e02dd561a8fca4495951c3c0
TDolphy <tyler.dolphy@gmail.com>
2017-03-13 17:18:15 -0400

    made api link secure

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ $(document).ready(function() {
       console.log(topic);
 
       // Constructing a URL to search Giphy for the name of the person who said the quote
-      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
        topic + "&api_key=dc6zaTOxFJmzC&limit=10";
     
     console.log(queryURL);
